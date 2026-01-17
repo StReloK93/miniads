1ba8f05c15339077d2077401a5967178d39f81e0
StReloK93 <strelok0493@gmail.com>
2026-01-17 12:58:25 +0500

    fast

--- resources/ts/pages/application/CategoryIdPage.vue
+++ resources/ts/pages/application/CategoryIdPage.vue
@@ -1,19 +1,12 @@
 <template>
    <div class="grid grid-cols-2 lg:grid-cols-3 gap-2 content-start">
       <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
-      <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
-      <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
-      <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
-      <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
-      <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
-      <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
-      <BaseProductCard v-for="product in data?.products || []" :key="product.id" :product />
    </div>
 </template>
 
 <script setup lang="ts">
 import BaseProductCard from "@/components/BaseProductCard.vue";
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import CategoryRepo from "@/entities/Category/CategoryRepo";
 import { useFetchDecorator } from "@/modules/useFetch";
 import { useRoute } from "vue-router";
--- resources/ts/pages/application/ProductPage.vue
+++ resources/ts/pages/application/ProductPage.vue
@@ -1,8 +1,16 @@
 <template>
+   <section class="container mx-auto">
+      <swiper :slides-per-view="1" :space-between="10">
+         <swiper-slide v-for="item in 10" :key="item">
+            <img :src="`https://picsum.photos/1200?random=${item}`" class="h-32 w-full" />
+         </swiper-slide>
+      </swiper>
+   </section>
    <div class="px-4">{{ product }}</div>
 </template>
 
 <script setup lang="ts">
+import { Swiper, SwiperSlide } from "swiper/vue";
 import { useRoute } from "vue-router";
 import ProductRepo from "@/entities/Product/ProductRepo";
 import { useFetchDecorator } from "@/modules/useFetch";
